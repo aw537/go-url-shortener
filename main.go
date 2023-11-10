538331ca470d92317eb1afd4674aa3612cb27289
Andres Wu <aw537@cornell.edu>
2023-11-09 23:16:38 -0500

    Created URL Handler and Finished Logic

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
+	"net/http"
 	"sync"
 )
 
@@ -18,4 +20,28 @@ func generateShortURL(longURL string) string {
 	hasher.Write([]byte(longURL))
 	shortURL := hex.EncodeToString(hasher.Sum(nil))[:8]
 	return shortURL
+}
+
+func shortenURLHandler(writer http.ResponseWriter, request *http.Request) {
+	// Parse the long URL from the request
+	longURL := request.URL.Query().Get("url")
+	if longURL == "" {
+		http.Error(writer, "Missing URL parameter", http.StatusBadRequest)
+		return
+	}
+
+	shortURL := generateShortURL(longURL)
+
+	// Lock the map for safe concurrent access
+	mapMutex.Lock()
+	urlMap[shortURL] = longURL
+	mapMutex.Unlock()
+
+	// Respond with the short URL
+	fmt.Fprintf(writer, "Short URL is: %s", shortURL)
+}
+
+func main() {
+	http.HandleFunc("/shorten", shortenURLHandler)
+	http.ListenAndServe(":8080", nil)
 }
